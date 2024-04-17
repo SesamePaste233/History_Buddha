6b6d31e07bd44f5ce30b4a9e481d0b7d5df77e2a
YXDXY <xinyiding00@gmail.com>
2024-04-17 11:30:37 +0200

    update manual adj

--- manual_ldmk_adjustment.py
+++ manual_ldmk_adjustment.py
@@ -4,6 +4,7 @@ import os
 
 import dash
 from dash import dcc, html, Input, Output, State
+from dash.exceptions import PreventUpdate
 import plotly.graph_objs as go
 import cv2
 import numpy as np
@@ -15,7 +16,9 @@ all_images = {}
 
 for file_path in glob.glob('extracted/ortho_landmarks/*.json'):
     file_name = os.path.basename(file_path)
-    key = "-".join(file_name.split("-")[:2])
+    key1 = "-".join(file_name.split("-")[:2])
+    key2 = file_name.split("-")[-1].split("_")[1]
+    key = key1 + "-" + key2
     dyn = file_name.split("-")[3].split("_")
     
     with open(file_path, 'r') as json_file:
@@ -23,47 +26,145 @@ for file_path in glob.glob('extracted/ortho_landmarks/*.json'):
 
 for file_path in glob.glob('extracted/orthogonalized/*.png'):
     file_name = os.path.basename(file_path)
-    key = "-".join(file_name.split("-")[:2])
+    key1 = "-".join(file_name.split("-")[:2])
+    key2 = file_name.split("-")[-1].split("_")[1]
+    key = key1 + "-" + key2
     all_images[key] = "extracted/orthogonalized/"+file_name
 
 # Load initial data
-idx = "13-11"
-image_path = all_images[idx]
-image = cv2.imread(image_path)
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-height, width, _ = image_rgb.shape
+# idx = "13-11"
+# image_path = all_images[idx]
+# image = cv2.imread(image_path)
+# image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# height, width, _ = image_rgb.shape
 
 app.layout = html.Div([
-    dcc.Graph(
-        id='image-graph',
-        figure={
-            'data': [
-                go.Image(z=image_rgb),
-                go.Scatter(
-                    x=[p[0] for p in all_ldmk_data[idx]],
-                    y=[p[1] for p in all_ldmk_data[idx]],
-                    mode='markers',
-                    marker=dict(color='red', size=12),
-                    name='Landmarks'
+    html.Div([  # Main container
+        dcc.Graph(
+            id='image-graph',
+            figure={
+                'data': [],
+                'layout': go.Layout(
+                    xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                    yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False, 'scaleanchor': "x", 'scaleratio': 1},
+                    margin={'l': 0, 'r': 0, 't': 0, 'b': 0},
+                    height=800,
+                    # width='60vh',  
+                    clickmode='event+select'
                 )
-            ],
-            'layout': go.Layout(
-                xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
-                yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False, 'scaleanchor': "x", 'scaleratio': 1},
-                margin={'l': 0, 'r': 0, 't': 0, 'b': 0},
-                height=800,
-                width=800,
-                clickmode='event+select'
-            )
-        },
-        style={'height': '90vh', 'width': '100%'}
-    ),
-    html.Button("Save Landmarks", id="save-button"),
-    html.Div(id='debug-info'),
+            },
+            style={'height': '90vh', 'width': '100%'}
+        ),
+        html.Div([  # Side panel for input and button
+            dcc.Input(id='input-index', type='text', placeholder='Enter Image Index',
+                      style={'margin': '5px', 'width': '250px', 'height': '40px'}),  
+            html.Button('Load Image', id='load-button',
+                        style={'margin': '5px', 'width': '150px', 'height': '45px'}),  
+            html.Button('Save Updated Landmarks', id='save-updated-button',  
+                style={'margin': '5px', 'width': '250px', 'height': '45px'}),
+        ], style={
+            'display': 'flex',
+            'flexDirection': 'column',  
+            'justifyContent': 'center',
+            'alignItems': 'left',
+            'width': '50%',  
+            'height': '90vh'
+        }),
+    ], style={
+        'display': 'flex',
+        'alignItems': 'stretch',
+        'width': '100%'  # Ensures the outer div takes full width of its parent
+    }),
+    html.Div(id='debug-info', style={
+        'position': 'absolute',  # Absolute position
+        'top': '10px',  
+        'right': '10px',  
+        'background': 'white',
+        'padding': '10px',
+        'border': '1px solid #ccc',
+        'border-radius': '5px'
+    }),
     dcc.Download(id="download-landmarks"),
-    dcc.Store(id='landmarks-store', data=all_ldmk_data[idx]),
+    dcc.Store(id='landmarks-store'),
     dcc.Store(id='selected-landmark-index', data=None)
-])
+], style={'display': 'flex', 'flexDirection': 'column', 'height': '100vh'})
+
+@app.callback(
+    [Output('image-graph', 'figure'),
+     Output('landmarks-store', 'data'),
+     Output('selected-landmark-index', 'data')],
+    [Input('load-button', 'n_clicks'),
+     Input('image-graph', 'clickData')],
+    [State('input-index', 'value'),
+     State('landmarks-store', 'data'),
+     State('selected-landmark-index', 'data'),
+     State('image-graph', 'figure')]
+)
+def update_content(load_clicks, clickData, input_idx, landmarks, selected_index, fig):
+    ctx = dash.callback_context
+
+    if not ctx.triggered:
+        raise PreventUpdate
+
+    trigger_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    if trigger_id == 'load-button':
+        if input_idx in all_images and input_idx in all_ldmk_data:
+            image_path = all_images[input_idx]
+            name = os.path.basename(image_path).split('.')[0]
+            image = cv2.imread(image_path)
+            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            landmarks = all_ldmk_data[input_idx]
+
+            fig = {
+                'data': [
+                    go.Image(z=image_rgb),
+                    go.Scatter(
+                        x=[p[0] for p in landmarks],
+                        y=[p[1] for p in landmarks],
+                        mode='markers+text',
+                        marker=dict(color='red', size=12),
+                        text=[str(i) for i in range(len(landmarks))],
+                        textposition="top center",
+                        name='Landmarks'
+                    )
+                ],
+                'layout': go.Layout(
+                    xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                    yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False, 'scaleanchor': "x", 'scaleratio': 1},
+                    margin={'l': 0, 'r': 0, 't': 0, 'b': 0},
+                    height=800,
+                    width=800,
+                    clickmode='event+select'
+                )
+            }
+            return fig, landmarks, dash.no_update  # Reset selected index only if needed
+        else:
+            raise PreventUpdate
+
+    elif trigger_id == 'image-graph' and clickData:
+        if clickData:
+            curve_idx = clickData['points'][0]['curveNumber']
+            data_type = fig['data'][curve_idx]['type']
+
+            x, y = clickData['points'][0]['x'], clickData['points'][0]['y']
+            distances = [(x - px) ** 2 + (y - py) ** 2 for px, py in landmarks]
+            closest_idx = np.argmin(distances)
+
+            if data_type == 'scatter':
+                fig['data'][1]['x'] = [p[0] for p in landmarks]
+                fig['data'][1]['y'] = [p[1] for p in landmarks]
+                selected_index = closest_idx
+            else:
+                landmarks[selected_index] = (x, y)
+                fig['data'][1]['x'] = [p[0] for p in landmarks]
+                fig['data'][1]['y'] = [p[1] for p in landmarks]
+                selected_index = None
+            print(selected_index, (x, y))
+            return fig, landmarks, selected_index
+        raise PreventUpdate
+
+    raise PreventUpdate
 
 @app.callback(
     Output('debug-info', 'children'),
@@ -73,54 +174,30 @@ def update_debug_info(selected_index):
     return f"Selected Landmark Index: {selected_index}"
 
 @app.callback(
-    [Output('image-graph', 'figure'), Output('selected-landmark-index', 'data')],
-    [Input('image-graph', 'clickData')],
-    [State('landmarks-store', 'data'), State('selected-landmark-index', 'data'), State('image-graph', 'figure')]
-)
-def update_landmarks(clickData, landmarks, selected_index, fig):
-    if clickData:
-
-        curve_idx = clickData['points'][0]['curveNumber']
-        data_type = fig['data'][curve_idx]['type']
-
-        x, y = clickData['points'][0]['x'], clickData['points'][0]['y']
-        distances = [(x - px) ** 2 + (y - py) ** 2 for px, py in landmarks]
-        closest_idx = np.argmin(distances)
-
-        if data_type == 'scatter':
-            fig['data'][1]['x'] = [p[0] for p in landmarks]
-            fig['data'][1]['y'] = [p[1] for p in landmarks]
-            selected_index = closest_idx
-        else:
-            landmarks[selected_index] = (x, y)
-            fig['data'][1]['x'] = [p[0] for p in landmarks]
-            fig['data'][1]['y'] = [p[1] for p in landmarks]
-            selected_index = None
-        print(selected_index, (x, y))
-        return fig, selected_index
-    return dash.no_update, dash.no_update
-
-@app.callback(
-    Output('landmarks-store', 'data'),
-    [Input('image-graph', 'figure')],
-    [State('landmarks-store', 'data')]
-)
-def store_landmarks(fig, landmarks):
-    landmarks = [(px, py) for px, py in zip(fig['data'][1]['x'], fig['data'][1]['y'])]
-    return landmarks
-
-@app.callback(
     Output('download-landmarks', 'data'),
-    [Input('save-button', 'n_clicks')],
-    [State('landmarks-store', 'data')]
+    [Input('save-updated-button', 'n_clicks')],
+    [State('input-index', 'value'),
+     State('landmarks-store', 'data')]
 )
-def save_landmarks(n_clicks, landmarks):
-    if n_clicks:
-        path = 'landmarks.json'
-        with open(path, 'w') as f:
-            json.dump(landmarks, f)
-        return dcc.send_file(path)
+def save_updated_landmarks(n_clicks, input_idx, landmarks):
+    if n_clicks and landmarks:
+        if input_idx in all_images:
+            original_filename = os.path.basename(all_images[input_idx]).split('.')[0]
+            updated_path = f'extracted/updated_landmarks/{original_filename}.json'
+
+            # Ensure the directory exists
+            os.makedirs(os.path.dirname(updated_path), exist_ok=True)
+
+            # Save the updated landmarks to the specified file
+            with open(updated_path, 'w') as f:
+                json.dump(landmarks, f)
+            
+            # Optionally, if you want to allow downloading the saved file:
+            return dcc.send_file(updated_path)
+        else:
+            return dash.no_update
     return dash.no_update
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
