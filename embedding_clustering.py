89296eb21547d1244abc605fe1f4d7893554e34a
SesamePaste233 <61174335+SesamePaste233@users.noreply.github.com>
2024-06-03 13:17:23 +0200

    greyscale embeddings

--- embedding_clustering.py
+++ embedding_clustering.py
@@ -5,10 +5,24 @@ import json
 
 from check_files import NameParser
 
+USE_GREYSCALE = True
+
 embeddings_folder = r'./extracted/embeddings'
+
+embeddings_greyscale_folder = r'./extracted/embeddings_greyscale'
+
+extra_embeddings_folder = r'./extraneous_buddha_statues/extra_embeddings'
+
+extra_embeddings_greyscale_folder = r'./extraneous_buddha_statues/extra_embeddings_greyscale'
+
+if USE_GREYSCALE:
+    embeddings_folder = embeddings_greyscale_folder
+    extra_embeddings_folder = extra_embeddings_greyscale_folder
 
 # Read embeddings
 embedding_files = [os.path.join(embeddings_folder, f) for f in os.listdir(embeddings_folder) if os.path.isfile(os.path.join(embeddings_folder, f))]
+
+extra_embeddings_files = [os.path.join(extra_embeddings_folder, f) for f in os.listdir(extra_embeddings_folder) if os.path.isfile(os.path.join(extra_embeddings_folder, f))]
 
 embeddings = []
 periods = []
@@ -23,39 +37,99 @@ for embedding_file in embedding_files:
         embedding = json.load(f)
         embeddings.append(embedding)
 
+extra_embeddings = []
+for extra_embedding_file in extra_embeddings_files:
+    file_name = os.path.splitext(os.path.basename(extra_embedding_file))[0]
+    periods.append(['extra'])
+
+    with open(extra_embedding_file, 'r') as f:
+        embedding = json.load(f)
+        extra_embeddings.append(embedding)
+
 # Convert embeddings to numpy array
 embeddings = np.array(embeddings)
+
+extra_embeddings = np.array(extra_embeddings)
+
+all_embeddings = np.concatenate((embeddings, extra_embeddings))
 
 # Apply UMAP to reduce dimensionality
 import umap
 
-umap_embeddings = umap.UMAP(n_neighbors=5, n_components=3, min_dist=0.3, metric='correlation').fit_transform(embeddings)
+_umap_embeddings = umap.UMAP(n_neighbors=5, n_components=2, min_dist=0.3, metric='correlation').fit_transform(all_embeddings)
 
 # Plot the clusters
 import matplotlib.pyplot as plt
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-for i in range(len(umap_embeddings)):
-    x, y, z = umap_embeddings[i]
-    period:str = periods[i]
-    is_wei = False
-    is_qi = False
-    for p in period:
-        if 'wei' in p.lower():
-            is_wei = True
-            break
-    if not is_wei:
+def draw_embeddings_3d(umap_embeddings):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for i in range(len(umap_embeddings)):
+        x, y, z = umap_embeddings[i]
+        period:str = periods[i]
+        is_wei = False
+        is_qi = False
+        is_extra = False
         for p in period:
-            if 'qi' in p.lower():
-                is_qi = True
+            if 'wei' in p.lower():
+                is_wei = True
                 break
+        if not is_wei:
+            for p in period:
+                if 'qi' in p.lower():
+                    is_qi = True
+                    break
+        if not is_wei and not is_qi:
+            for p in period:
+                if 'extra' in p.lower():
+                    is_extra = True
+                    break
 
-    if is_wei:
-        ax.scatter(x, y, z, c='r', marker='o')
-    elif is_qi:
-        ax.scatter(x, y, z, c='g', marker='o')
-    else:
-        ax.scatter(x, y, z, c='b', marker='o')
+        if is_wei:
+            ax.scatter(x, y, z, c='r', marker='o')
+        elif is_qi:
+            ax.scatter(x, y, z, c='g', marker='o')
+        elif is_extra:
+            ax.scatter(x, y, z, c='y', marker='o')
+        else:
+            #ax.scatter(x, y, z, c='b', marker='o')
+            pass
 
-plt.show()
+    plt.show()
+
+def draw_embeddings_2d(umap_embeddings):
+    for i in range(len(umap_embeddings)):
+        x, y = umap_embeddings[i]
+        period:str = periods[i]
+        is_wei = False
+        is_qi = False
+        is_extra = False
+        for p in period:
+            if 'wei' in p.lower():
+                is_wei = True
+                break
+        if not is_wei:
+            for p in period:
+                if 'qi' in p.lower():
+                    is_qi = True
+                    break
+        if not is_wei and not is_qi:
+            for p in period:
+                if 'extra' in p.lower():
+                    is_extra = True
+                    break
+
+        if is_wei:
+            plt.scatter(x, y, c='r', marker='o')
+        elif is_qi:
+            plt.scatter(x, y, c='g', marker='o')
+        elif is_extra:
+            plt.scatter(x, y, c='y', marker='o')
+        else:
+            #plt.scatter(x, y, c='b', marker='o')
+            pass
+
+    plt.show()
+
+
+draw_embeddings_2d(_umap_embeddings)
--- face_embedding.py
+++ face_embedding.py
@@ -3,16 +3,37 @@ import os
 from PIL import Image
 import numpy as np
 import json
+import cv2
 
-embeddings_folder = r'./extracted/embeddings'
+embeddings_folder = r'./extracted/embeddings_greyscale'
+
+extra_embeddings_folder = r'./extraneous_buddha_statues/extra_embeddings_greyscale'
 
 ortho_face_folder = r'./extracted/faces'
 
+extra_face_folder = r'./extraneous_buddha_statues/extra_faces'
+
+# mkdir
+os.makedirs(embeddings_folder, exist_ok=True)
+os.makedirs(extra_embeddings_folder, exist_ok=True)
+
+
 face_image_paths = [os.path.join(ortho_face_folder, f) for f in os.listdir(ortho_face_folder) if os.path.isfile(os.path.join(ortho_face_folder, f))]
 
-def extract_face_embeddings(face_image_path, embeddings_folder):
+extra_face_image_paths = [os.path.join(extra_face_folder, f) for f in os.listdir(extra_face_folder) if os.path.isfile(os.path.join(extra_face_folder, f)) and not f.endswith('.rtf')]
+
+def extract_face_embeddings(face_image_path, embeddings_folder, use_greyscale=False):
     file_name = os.path.splitext(os.path.basename(face_image_path))[0]
-    face_embedding = DeepFace.represent(face_image_path, model_name='Facenet', detector_backend='retinaface')
+
+    image = cv2.imread(face_image_path)
+    if image is None:
+        raise ValueError(f'Failed to read image from {face_image_path}.')
+
+    if use_greyscale:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+
+    face_embedding = DeepFace.represent(image, model_name='Facenet', detector_backend='retinaface')
     face_embedding_path = os.path.join(embeddings_folder, f'{file_name}.json')
 
     assert len(face_embedding) == 1, f'Expected 1 face embedding, but got {len(face_embedding)} for {face_image_path}.'
@@ -20,10 +41,23 @@ def extract_face_embeddings(face_image_path, embeddings_folder):
     with open(face_embedding_path, 'w') as f:
         json.dump(face_embedding[0]['embedding'], f)
 
-for face_image_path in face_image_paths:
-    try:
-        extract_face_embeddings(face_image_path, embeddings_folder)
-    except Exception as e:
-        print(f'Failed to extract face embedding for {face_image_path}.')
-        print(e)
-        continue
+if __name__ == '__main__':
+    from tqdm import tqdm
+    for extra_face_image_path in tqdm(extra_face_image_paths):
+        try:
+            extract_face_embeddings(extra_face_image_path, extra_embeddings_folder, use_greyscale=True)
+        except Exception as e:
+            print(f'Failed to extract face embedding for {extra_face_image_path}.')
+            print(e)
+            continue
+    '''
+    for face_image_path in tqdm(face_image_paths):
+        try:
+            extract_face_embeddings(face_image_path, embeddings_folder)
+        except Exception as e:
+            print(f'Failed to extract face embedding for {face_image_path}.')
+            print(e)
+            continue
+    '''
+    
+    
